2b1a93c16a553aafe25f59934bc0946d46755535
bear320 <aaabear320@gmail.com>
2024-10-02 20:21:15 +0800

    feat: 🎸 Set up UI for authentication

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,13 +1,20 @@
 import { Editor } from "@/components/editor/Editor";
 import Header from "@/components/Header";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 const Document = () => {
   return (
     <div>
       <Header className="">
         <div className="flex w-fit items-center justify-center gap-2">
-          <p className="document-title">This is a fake document title</p>
+          <p className="document-title">share</p>
         </div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </Header>
       <Editor />
     </div>
