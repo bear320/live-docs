2c63360ae7f2da4bafd8dd08a80d7e6b6062e7a4
bear320 <aaabear320@gmail.com>
2024-10-05 20:09:18 +0800

    feat: 🎸 Add collaborators and display them in the document

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -1,25 +1,32 @@
 "use client";
 
 import Header from "@/components/Header";
+import ActiveCollaborators from "./ActiveCollaborators";
 import { Editor } from "@/components/editor/Editor";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { ClientSideSuspense, RoomProvider } from "@liveblocks/react/suspense";
 
-const CollaborativeRoom = () => {
+const CollaborativeRoom = ({
+  roomId,
+  roomMetadata,
+}: CollaborativeRoomProps) => {
   return (
-    <RoomProvider id="my-room">
+    <RoomProvider id={roomId}>
       <ClientSideSuspense fallback={<div>Loading…</div>}>
         <div className="collaborative-room">
           <Header className="">
             <div className="flex w-fit items-center justify-center gap-2">
               <p className="document-title">share</p>
             </div>
-            <SignedOut>
-              <SignInButton />
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
+            <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
+              <ActiveCollaborators />
+              <SignedOut>
+                <SignInButton />
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </div>
           </Header>
           <Editor />
         </div>
