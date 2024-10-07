a0c0a8ba3455532a7158eb069a694c66def418b4
bear320 <aaabear320@gmail.com>
2024-10-07 20:09:39 +0800

    feat: 🎸 Display all documents on homepage

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -36,6 +36,18 @@ export const createDocument = async ({
   }
 };
 
+export const getDocuments = async (email: string) => {
+  try {
+    const rooms = await liveblocks.getRooms({
+      userId: email,
+    });
+
+    return parseStringify(rooms);
+  } catch (error) {
+    console.log(`Error happened while getting rooms: ${error}`);
+  }
+};
+
 export const getDocument = async ({
   roomId,
   userId,
