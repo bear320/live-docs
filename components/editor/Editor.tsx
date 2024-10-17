6dbe3813ef18d16079876acf81b5214c031658f0
bear320 <aaabear320@gmail.com>
2024-10-17 23:23:40 +0800

    feat: 🎸 Add delete modal and button in the document

--- components/editor/Editor.tsx
+++ components/editor/Editor.tsx
@@ -20,6 +20,7 @@ import Loader from "../Loader";
 import FloatingToolbarPlugin from "./plugins/FloatingToolbarPlugin";
 import { useThreads } from "@liveblocks/react/suspense";
 import Comments from "../Comments";
+import DeleteModal from "../DeleteModal";
 
 // Catch any errors that occur during Lexical updates and log them
 // or throw them as needed. If you don't throw them, Lexical will
@@ -55,7 +56,7 @@ export const Editor = ({
       <div className="editor-container size-full">
         <div className="toolbar-wrapper justifyy-between flex min-w-full">
           <ToolbarPlugin />
-          {/* {currentUserType === "editor" && <DeleteModal roomId={roomId} />} */}
+          {currentUserType === "editor" && <DeleteModal roomId={roomId} />}
         </div>
 
         <div className="editor-wrapper flex flex-col items-center justify-start">
